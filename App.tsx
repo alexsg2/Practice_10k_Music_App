f2d37a3fb2e5f2e9e7e431e4a5b479098a5001f8
Bailey McKelway <bjlmckelway@gmail.com>
2023-09-21 13:19:22 -0400

    adding THIS IS A TEST comment

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // THIS IS A TEST
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
