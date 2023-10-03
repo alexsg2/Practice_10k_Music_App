38571f17fddfe187a3dc22f9332548be8c35495a
ceholmes <56744477+ceholmes@users.noreply.github.com>
2023-10-03 14:38:14 -0400

    Added tab navigation for signed-in view

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -6,6 +6,7 @@ import { Ionicons } from '@expo/vector-icons'
 
 // Import our custom screens here
 import JournalScreen from './screens/JournalScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,10 @@ function AppNavigation() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: (tabInfo) => (
+            <Ionicons name="home-outline" size={24} color={tabInfo.focused ? "#5982C2" : "#000000"} />
+          ),
+        }}/>
         <Tab.Screen name="Journal" component={JournalScreen} options={{
           tabBarIcon: (tabInfo) => (
             <Ionicons name="journal-outline" size={24} color={tabInfo.focused ? "#5982C2" : "#000000"} />
