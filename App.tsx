e2f0d1634dc2bafeec3a7b7655da3b3150a82c9d
Bailey McKelway <bjlmckelway@gmail.com>
2023-10-04 13:42:39 -0400

    start

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './config/firebase';
 import RootNavigation from './navigation';
-import AuthNavigator from './src/screens/auth/AuthNavigator';
 
 export default function App() {
-  return (
-    <RootNavigation/>
-  );
+  return <RootNavigation />;
 }
