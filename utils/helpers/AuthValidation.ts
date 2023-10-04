c2b7daa712682a2609dd2828bfe3aa952904b719
malizemui <mtzemui@vt.edu>
2023-10-04 12:02:37 -0400

    fixed library versions

--- utils/helpers/AuthValidation.ts
+++ utils/helpers/AuthValidation.ts
@@ -7,12 +7,12 @@
 export function validateRegistrationFormat(name: string, email: string, password: string): string | null
 {
     if (!name || !email || !password) {
-        return 'Name, email and password are mandatory.';
+        return 'Name, email and password are required.';
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return 'Invalid email address.';
+        return 'Email format is invalid.';
     }
   
     if (password.length < 8) {
@@ -25,12 +25,12 @@ export function validateRegistrationFormat(name: string, email: string, password
 export function validateLoginFormat(email: string, password: string): string | null
 {
     if (!email || !password) {
-        return 'Email and password are mandatory.';
+        return 'Email and password are required.';
     }
   
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return 'Invalid email address.';
+        return 'Email format is invalid.';
     }
   
     return null; // No validation errors
