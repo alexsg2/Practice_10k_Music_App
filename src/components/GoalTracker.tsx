48af0374d7c55f5c14c2f267437fa675f244d85f
alexsg2 <alex.s.georgiev@gmail.com>
2023-10-17 14:51:19 -0400

    Progress_Tracking_Implementation_Start

---
+++ src/components/GoalTracker.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { CircularProgress } from 'react-native-circular-progress';
+import { DesignLibrary } from '../assets/DesignLibrary';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+const ProgressTracker = ({ title = "", goal_amount = 0, hours_amount = 0, pieces_amount = 0 }) => {
+  const goal = goal_amount;
+  const hours_done = hours_amount;
+  const hoursRemaining = goal - hours_done;
+  const pieces = pieces_amount;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={{ fontSize: 16 }}>{title}</Text>
+        <Text style={{ marginBottom: 15, fontSize: 14 }}>Remaining = Goal - Hours</Text>
+
+
+        <View style={styles.progressContainer}>
+          <CircularProgress
+            size={100}
+            width={10}
+            fill={100 - (hoursRemaining / goal) * 100}
+            tintColor="#7BC3E9"
+            backgroundColor="#3D5AA5"
+            rotation={0}
+          >
+            {() => (
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ fontSize: 20 }}>{`${hoursRemaining}`}</Text>
+                <Text style={{ fontSize: 10 }}>Remaining</Text>
+              </View>
+            )}
+          </CircularProgress>
+        </View>
+      </View>
+      <View style={styles.rightContainer}>
+        <View style={styles.iconTextContainer}>
+          <View style={styles.iconContainer}>
+            <FontAwesome5 name="font-awesome-flag" size={24} color="#E74E4E" />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={{ fontSize: 16, fontWeight: '300' }}>Goal</Text>
+            <Text style={{ marginBottom: 20, fontSize: 16, fontWeight: '600' }}>{goal} hours</Text>
+          </View>
+        </View>
+        <View style={styles.iconTextContainer}>
+          <View style={styles.iconContainer}>
+            <MaterialIcons name="queue-music" size={24} color="#7F079D" />
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={{ fontSize: 16, fontWeight: '300' }}>Pieces</Text>
+            <Text style={{ fontSize: 16, fontWeight: '600' }}>{pieces}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    height: 200,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    borderWidth: 2, // Add a black border
+    borderColor: DesignLibrary.color_pallete.black["default"], // Set the border color to black
+  },
+  leftContainer: {
+    flex: 3.5,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  progressContainer: {
+    width: 100, // Set your desired size for the progress bar
+    height: 100,
+  },
+  rightContainer: {
+    flex: 2,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconTextContainer: {
+    flexDirection: 'row',
+  },
+  iconContainer: {
+    marginRight: 15, // Adjust the spacing between icon and text
+  },
+
+  textContainer: {
+    flex: 1, // Allow text to expand to fill available space
+  },
+});
+
+export default ProgressTracker;
--- src/screens/tabs/Progress.tsx
+++ src/screens/tabs/Progress.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import GoalTracker from '../../components/GoalTracker'
 
 export default function Progress() {
     return (
-        <View style={styles.container}>
-            <Text>Temporary progress screen.</Text>
-            <StatusBar style="auto" />
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.container}>
+                <GoalTracker title="Daily Hours" goal_amount={10} hours_amount={7} pieces_amount={2} />
+                <GoalTracker title="Weekly Hours" goal_amount={20} hours_amount={12} pieces_amount={26} />
+                <GoalTracker title="Monthly Hours" goal_amount={50} hours_amount={24} pieces_amount={55} />
+                <GoalTracker title="Overall Hours" goal_amount={200} hours_amount={107} pieces_amount={123} />
+
+                <Text>Temporary progress screen.</Text>
+                <StatusBar style="auto" />
+            </View>
+        </ScrollView>
     );
 }
 
