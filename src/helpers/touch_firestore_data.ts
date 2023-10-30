da7c08c910c2409262ca0238dba9393fbb6598d1
rkaneja <rahulaneja1121@gmail.com>
2023-10-30 13:04:49 -0400

    functions set up. not tested tho.

--- src/helpers/touch_firestore_data.ts
+++ src/helpers/touch_firestore_data.ts
@@ -3,7 +3,7 @@
  */
 
 import { db } from '../config/firebase';
-import { doc, setDoc, collection, getDocs, deleteDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, getDocs, deleteDoc, query, where, and, updateDoc } from 'firebase/firestore';
 
 
 import { IProfileProps } from "../redux/reducers"
@@ -71,3 +71,36 @@ export const addPracticeData = async (userId: string, title: string, piece: stri
 
 
 // TODO : create a function to --> fetch practice data by date range, update pratice data and delete practice data
+export const getPracticeData = async (userId: string, dateStart: Date, dateEnd:Date) => 
+{
+    const q = query(collection(db, 'users', userId, 'practiceLogs'), and(where('date', ">=", dateStart), where('date', '<=', dateEnd)));
+    try{
+        // query practiceLog collection to get practice logs by date range given
+        // TODO this should have the practice logs of the user by the date
+        const querySnapshot = await getDocs(q);
+        return querySnapshot;
+    }
+    catch(e){
+        console.log("something went wrong getting the practice. error: " + e);
+    }
+}
+
+export const updatePracticeData = async (userId: string, practiceLogId: string, data: any) => {
+    const practiceLogRef = doc(db, userId, "practiceLogs", practiceLogId);
+    try{
+        await updateDoc(practiceLogRef, data);
+    }
+    catch(e){
+        console.log("something went wrong with updating. error: " + e);
+    }
+}
+
+export const deletePracticeData = async (userId: string, practiceLogId: string) => {
+    const practiceLogRef = doc(db, userId, "practiceLogs", practiceLogId);
+    try{
+        await deleteDoc(practiceLogRef);
+    }
+    catch(e){
+        console.log("something went wrong with deleting. error: " + e);
+    }
+}
